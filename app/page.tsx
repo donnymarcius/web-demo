3f3dde92dac3827a5f2ce34c8d28a4f17aec83f2
donnymarcius <donnymarcius@gmail.com>
2024-09-17 22:12:02 +0700

    update header

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,37 @@
 export default function Home() {
   return (
-    <div className="px-4">
-      <section className="flex flex-col text-center">
-        <h1 className="text-9xl font-bold">Synbio ID</h1>
-        <p>
-          A Non-profit Organization that Focus on Enhancing Synthetic Biology and Bioinformatics Enthusiasts in Indonesia. We are Ready to Make Any Collaborations with Your Institution.
+    <div>
+      <section className="px-4 h-[calc(90dvh)] flex flex-col text-center justify-center items-center">
+        <h1
+          className="text-9xl font-bold my-6"
+          style={{ 
+            backgroundImage: 'linear-gradient(to right, var(--synbio-green), var(--natural-green))',
+            WebkitBackgroundClip: 'text',
+            color: 'transparent',
+          }}
+        >
+          Synbio ID
+        </h1>
+        <p className="text-lg">
+          A Non-profit Organization that Focus on Enhancing Synthetic Biology and <br />
+          Bioinformatics Enthusiasts in Indonesia. We are Ready to Make Any <br />
+          Collaborations with Your Institution.
         </p>
       </section>
+
+      <div
+        className="relative h-[200px]"
+        style={{
+          backgroundImage: "linear-gradient(to right, var(--synbio-green), var(--natural-green), transparent), url('/images/images.jpg')",
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      >
+        <div className="absolute top-1/2 transform -translate-y-1/2 text-white pl-4 pr-64">
+          <h2>Welcome to My Website</h2>
+          <p>This is a container with a gradient background that transitions to an image.</p>
+        </div>
+      </div>
     </div>
   );
 }
