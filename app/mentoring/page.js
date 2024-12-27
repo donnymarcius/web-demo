7078a8fb484f8d883ccfd64fc1c96565ae442fb8
donnymarcius <donnymarcius@gmail.com>
2024-12-28 01:13:11 +0700

    fix issue

--- app/mentoring/page.js
+++ app/mentoring/page.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import React from 'react';
 import Image from 'next/image';
 import { useSession } from 'next-auth/react';
-import { CategoryData, CategoryProps } from '../data/cardCategory';
+import { CategoryData } from '../data/cardCategory';
 import { TestimonialData } from '../data/cardTestimony';
 import TestimonialCarousel from '../components/TestimonialCarousel';
 
